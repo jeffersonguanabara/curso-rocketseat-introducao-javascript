4d3e5651c9aefbd23c1383d988b94be7d68b1e27
jeffersonguanabara <jefferson.guanabara@dcx.ufpb.br>
2019-09-20 10:49:26 -0300

    solução da questão 2 do desafio do modulo 4

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
-function checaIdade(idade) {
-    return new Promise(function(resolve, reject) {
-        if (idade >= 18) {
-            resolve();
-        } else {
-            reject();
-        }
-    });
+var adicionar = function () {
+    var inputElement = document.querySelector('#app input');
+    var valor = inputElement.value;
+    
+    var pesquisar = function() {
+        return new Promise(function(resolve, reject) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', 'http://api.github.com/users/' + valor + '/repos');
+            xhr.send(null);
+            
+            xhr.onreadystatechange = function() {
+                if(xhr.readyState === 4) {
+                    if(xhr.status === 200) {
+                        resolve(JSON.parse(xhr.responseText));
+                    } else {
+                        reject('Erro na requisição!');
+                    }
+                }
+            }
+        });
+    }
+
+    pesquisar()
+        .then(function(response) {
+            var texto = 'Repositórios no GitHub de: ' + valor;
+            var paragrafo = document.createElement('p');
+            var paragrafoText = document.createTextNode(texto);
+            paragrafo.appendChild(paragrafoText);
+            document.querySelector('#lista').appendChild(paragrafo);
+
+            var listElement = document.createElement('ul');
+            for (todo of response) {
+                var todoElement = document.createElement('li');
+                var todoText = document.createTextNode(todo.name);
+                todoElement.appendChild(todoText);
+                listElement.appendChild(todoElement);
+            }
+
+            document.querySelector('#lista').appendChild(listElement);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
 }
 
-checaIdade(20)
-    .then(function() {
-        console.log("Maior que 18");
-    })
-    .catch(function() {
-        console.log("Menor que 18");
-    });
